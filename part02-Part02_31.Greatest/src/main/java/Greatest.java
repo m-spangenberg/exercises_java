69b11fd696e368cb24b0016444b46508f342465e
m-spangenberg <mspangenberg.dev@gmail.com>
2022-07-11 00:56:41 +0200

    chore: exercises

--- part02-Part02_31.Greatest/src/main/java/Greatest.java
+++ part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -3,8 +3,18 @@ public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
         //write some code here
+        int largerNumber = 0;
+        if (number1 > number2) {
+            largerNumber = number1;
+        } else {
+            largerNumber = number2;
+        }
 
-        return -1;
+        if (largerNumber > number3) {
+            return largerNumber;
+        } else {
+            return number3;
+        }
     }
 
     public static void main(String[] args) {
