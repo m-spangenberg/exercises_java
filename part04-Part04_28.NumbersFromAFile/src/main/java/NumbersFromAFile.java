0d246e5a20c3a6612399349dfa81439e1dc01e96
m-spangenberg <mspangenberg.dev@gmail.com>
2022-07-19 14:11:55 +0200

    chore: exercise

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -6,6 +6,8 @@ public class NumbersFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int lineValue = 0;
 
         System.out.print("File? ");
         String file = scanner.nextLine();
@@ -14,6 +16,21 @@ public class NumbersFromAFile {
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
 
+        try (Scanner filescanner = new Scanner(Paths.get(file))) {
+            while (filescanner.hasNextLine()) {
+
+                lineValue = Integer.valueOf(filescanner.nextLine());
+
+                if (lineValue <= upperBound && lineValue >= lowerBound) {
+                    count++;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("File cannot be read.");
+        }
+        
+        System.out.println("Numbers: " + count);
+        scanner.close();
     }
 
 }
