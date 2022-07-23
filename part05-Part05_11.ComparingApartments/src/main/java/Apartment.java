3cf669ea134409d60e1e4cabe6cbd620487fe12e
m-spangenberg <mspangenberg.dev@gmail.com>
2022-07-24 01:14:26 +0200

    chore: exercises

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -3,12 +3,40 @@ public class Apartment {
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
-        this.princePerSquare = pricePerSquare;
+        this.pricePerSquare = pricePerSquare;
+    }
+
+    public boolean largerThan(Apartment compared) {
+        if (this.squares > compared.squares) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int priceDifference(Apartment compared) {
+        int apartmentA = (this.squares * this.pricePerSquare);
+        int apartmentB = (compared.squares * compared.pricePerSquare);
+        if (apartmentA > apartmentB) {
+            return apartmentA - apartmentB;
+        } else {
+            return apartmentB - apartmentA;
+        }   
+    }
+
+    public boolean moreExpensiveThan(Apartment compared) {
+        int apartmentA = (this.squares * this.pricePerSquare);
+        int apartmentB = (compared.squares * compared.pricePerSquare);
+        if (apartmentA > apartmentB) {
+            return true;
+        } else {
+            return false;
+        }   
     }
 
 }
