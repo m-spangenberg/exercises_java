0fbede1ad4c26d2dc558431e5927ebb4b1e10c74
m-spangenberg <mspangenberg.dev@gmail.com>
2022-07-25 21:03:32 +0200

    chore: exercises

--- part06-Part06_01.Menu/src/main/java/Menu.java
+++ part06-Part06_01.Menu/src/main/java/Menu.java
@@ -3,11 +3,32 @@ import java.util.ArrayList;
 
 public class Menu {
 
+    // instance variables for menu object
     private ArrayList<String> meals;
 
+    // constructor creating new menu object
     public Menu() {
         this.meals = new ArrayList<>();
     }
 
-    // implement the required methods here
+    // add menu items to menu
+    public void addMeal(String meal) {
+
+        if (!(meals.contains(meal))) {
+            this.meals.add(meal);
+        }
+    }
+    
+    // remove menu items from menu
+    public void clearMenu() {
+        meals.clear();
+    }
+
+    // print contents of menu
+    public void printMeals() {
+        for (String meal : meals) {
+            System.out.println(meal);
+        }
+    }
+
 }
