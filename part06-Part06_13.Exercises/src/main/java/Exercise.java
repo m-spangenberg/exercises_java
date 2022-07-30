55a8ba42b824f44e4a6a8ddf8ad4daa519697d00
m-spangenberg <mspangenberg.dev@gmail.com>
2022-07-30 10:54:50 +0200

    chore: exercises

---
+++ part06-Part06_13.Exercises/src/main/java/Exercise.java
@@ -0,0 +1,27 @@
+import java.security.PublicKey;
+
+import javax.swing.UIDefaults.ProxyLazyValue;
+
+public class Exercise {
+    
+
+    private String name;
+    private boolean completed;
+
+    public Exercise(String name) {
+        this.name = name;
+        this.completed = false;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+}
