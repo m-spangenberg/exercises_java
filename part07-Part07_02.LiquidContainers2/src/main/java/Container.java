67b37c4d056168da734fc2bbc4b40be2d34cd32e
m-spangenberg <mspangenberg.dev@gmail.com>
2022-08-01 20:22:32 +0200

    chore: exercises

--- part07-Part07_02.LiquidContainers2/src/main/java/Container.java
+++ part07-Part07_02.LiquidContainers2/src/main/java/Container.java
@@ -2,31 +2,8 @@ public class Container {
 
     //instance variables
 
-    // constructor
+    // parameter free constructor
+
 
     // instance methods
-
-    public void add(int amount) {
-        if (!((amount + container1) > container1)) {
-            container1 += amount;
-        } else {
-            container1 = 100;
-        }
-    }
-
-    public void move(int amount) {
-        if (!(container1 > container2)) {
-            container2 += container1;
-        } else {
-            container1 = 100;
-        }
-    }
-
-    public void remove(int amount) {
-        if (!(amount > 100)) {
-            container -= amount;
-        } else {
-            container = 0;
-        }
-    }
 }
