37e1c81b4e2460699b20814d8417e2996a74774d
m-spangenberg <mspangenberg.dev@gmail.com>
2022-08-02 11:39:47 +0200

    chore: exercises

--- part07-Part07_02.LiquidContainers2/src/main/java/Container.java
+++ part07-Part07_02.LiquidContainers2/src/main/java/Container.java
@@ -1,9 +1,39 @@
 public class Container {
 
     //instance variables
+    private Integer contents;
 
     // parameter free constructor
-
+    public Container () {
+        this.contents = 0;
+    }
 
     // instance methods
+    public int contains() {
+        return this.contents;
+    }
+
+    public void add(int amount) {
+        if (!(amount < 0)) {
+            if (!(this.contents + amount > 100)) {
+                this.contents += amount;
+            } else {
+                this.contents = 100;
+            }
+        }
+    }
+
+    public void remove(int amount) {
+        if (!(amount < 0)) {
+            if (!(this.contents - amount < 0)) {
+                this.contents -= amount;
+            } else {
+                this.contents = 0;
+            }
+        }
+    }
+
+    public String toString() {
+        return this.contents + "/100";
+    }
 }
--- part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -6,14 +6,41 @@ public class LiquidContainers2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        Container first = new Container();
+        Container second = new Container();
+
         while (true) {
-            System.out.print("> ");
+
+            System.out.println("First: " + first);
+            System.out.println("Second: " + second);
 
             String input = scan.nextLine();
+            
             if (input.equals("quit")) {
                 break;
             }
 
+            String[] parts = input.split(" ");
+            int amount = Integer.valueOf(parts[1]);
+            String command = parts[0];
+
+            switch (command) {
+                case "add":
+                    first.add(amount);
+                    break;
+                case "move":
+                    if (!(amount > first.contains())) {
+                        first.remove(amount);
+                        second.add(amount);
+                    } else {
+                        second.add(first.contains());
+                        first.remove(amount);
+                    }
+                    break;
+                case "remove":
+                    second.remove(amount);
+                    break;
+            }
         }
     }
 
