19536a8c3d3a5cb919209553c3e42802f60bbcbe
m-spangenberg <mspangenberg.dev@gmail.com>
2022-08-02 21:56:37 +0200

    chore: exercises

--- part07-Part07_05.Searching/src/main/java/Searching.java
+++ part07-Part07_05.Searching/src/main/java/Searching.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -44,10 +43,41 @@ public class Searching {
     }
 
     public static int linearSearch(ArrayList<Book> books, int searchedId) {
+        int i = 0;
+        for (Book book : books) {
+            if (book.getId() == searchedId) {
+                return i;
+            }
+            i++;
+        }
         return -1;
     }
 
     public static int binarySearch(ArrayList<Book> books, long searchedId) {
+        
+        int left = 0;
+        int right = books.size() - 1;
+
+        while (true) {
+            if (left > right) {
+                break;
+            }
+
+            int middle = (left + right) / 2;
+
+            if (books.get(middle).getId() == searchedId) {
+                return middle;
+            }
+
+            if (books.get(middle).getId() < searchedId) {
+                left = middle + 1;
+            }
+
+            if (books.get(middle).getId() > searchedId) {
+                right = middle - 1;
+            }
+        }
+
         return -1;
     }
 }
