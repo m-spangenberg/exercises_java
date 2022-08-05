2fc3dc0733f7a1fc07509d96de65024a0e828c5d
m-spangenberg <mspangenberg.dev@gmail.com>
2022-08-05 12:35:29 +0200

    chore: exercises

--- part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
+++ part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
@@ -1,6 +1,4 @@
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RecipeSearch {
@@ -9,5 +7,30 @@ public class RecipeSearch {
         Scanner scanner = new Scanner(System.in);
 
     }
+
+    // UI
+        // read recipe file and place it into memory
+        // a way to retrieve recipes from file...
+        // File to read: recipes.txt
+            // a way to store recipes in memory
+            // RecipeBook.java
+
+        // list recipes in the following format...
+        // Recipes:
+        // Pancake dough, cooking time: 60
+        // Meatballs, cooking time: 20
+        // Tofu rolls, cooking time: 30and stop
+
+        // a way to search for recipes based on: name, cooking time, ingredients
+
+        // find recipe by name
+        // Enter command: find name
+        // Searched word: roll
+            // Recipes:
+            // Tofu rolls, cooking time: 30
+    
+        // Enter command:  stop
+    
+        
 
 }
